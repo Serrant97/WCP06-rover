2183f081af8b65b2d87a8629e500119093ac7d42
Serrant97 <32850434+Serrant97@users.noreply.github.com>
2019-05-03 13:02:56 -0400

    Add files via upload

--- PiMotor.py
+++ PiMotor.py
@@ -22,8 +22,8 @@ class Motor:
     config = int defining which pins control "forward" and "backward" movement.
     '''
     motorpins = {"MOTOR4":{"config":{1:{"e":32,"f":24,"r":26},2:{"e":32,"f":26,"r":24}},"arrow":1},
-                 "MOTOR3":{"config":{1:{"e":19,"f":21,"r":23},2:{"e":19,"f":23,"r":21}}, "arrow":2},
-                 "MOTOR2":{"config":{1:{"e":22,"f":16,"r":18},2:{"e":22,"f":18,"r":16}}, "arrow":3},
+                 "MOTOR3":{"config":{2:{"e":19,"f":21,"r":23},1:{"e":19,"f":23,"r":21}}, "arrow":2},
+                 "MOTOR2":{"config":{2:{"e":22,"f":16,"r":18},1:{"e":22,"f":18,"r":16}}, "arrow":3},
                  "MOTOR1":{"config":{1:{"e":11,"f":15,"r":13},2:{"e":11,"f":13,"r":15}},"arrow":4}}
     
     def __init__(self, motor, config):
@@ -56,7 +56,6 @@ class Motor:
         speed = Duty Cycle Percentage from 0 to 100.
         0 - stop and 100 - maximum speed
         '''    
-        print("Forward")
         if self.testMode:
             self.arrow.on()
         else:
@@ -71,7 +70,6 @@ class Motor:
         speed = Duty Cycle Percentage from 0 to 100.
         0 - stop and 100 - maximum speed
      '''
-        print("Reverse")
         if self.testMode:
             self.arrow.off()
         else:
@@ -82,7 +80,6 @@ class Motor:
     def stop(self):
         ''' Stops power to the motor,
      '''
-        print("Stop")
         self.arrow.off()
         self.PWM.ChangeDutyCycle(0)
         GPIO.output(self.pins['f'],GPIO.LOW)
---
+++ gyroDecode.c
@@ -0,0 +1,70 @@
+#include <wiringPiI2C.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <wiringPi.h>
+
+#define Device_Address 0x68	/*Device Address/Identifier for MPU6050*/
+
+#define PWR_MGMT_1   0x6B
+#define SMPLRT_DIV   0x19
+#define CONFIG       0x1A
+#define GYRO_CONFIG  0x1B
+#define INT_ENABLE   0x38
+#define ACCEL_YOUT_H 0x3D
+
+int fd;
+
+void MPU6050_Init(){
+	
+	wiringPiI2CWriteReg8 (fd, SMPLRT_DIV, 0x07);	/* Write to sample rate register */
+	wiringPiI2CWriteReg8 (fd, PWR_MGMT_1, 0x01);	/* Write to power management register */
+	wiringPiI2CWriteReg8 (fd, CONFIG, 0);		/* Write to Configuration register */
+	wiringPiI2CWriteReg8 (fd, GYRO_CONFIG, 24);	/* Write to Gyro Configuration register */
+	wiringPiI2CWriteReg8 (fd, INT_ENABLE, 0x01);	/*Write to interrupt enable register */
+
+	} 
+short read_raw_data(int addr){
+	short high_byte,low_byte,value;
+	high_byte = wiringPiI2CReadReg8(fd, addr);
+	low_byte = wiringPiI2CReadReg8(fd, addr+1);
+	value = (high_byte << 8) | low_byte;
+	return value;
+}
+
+void ms_delay(int val){
+	int i,j;
+	for(i=0;i<=val;i++)
+		for(j=0;j<1200;j++);
+}
+
+int main(){
+	
+	float Acc_y;
+	float Ay=0;
+	fd = wiringPiI2CSetup(Device_Address);   /*Initializes I2C with device Address*/
+	MPU6050_Init();		                 /* Initializes MPU6050 */
+	
+	while(1)
+	{
+		/* Read raw value of Accelerometer and gyroscope from MPU6050*/
+		Acc_y = read_raw_data(ACCEL_YOUT_H);
+		
+		/* Divide raw value by sensitivity scale factor */
+		Ay = Acc_y/16384.0;
+		
+		// printf("\nAy = %.3f g\n",Ay);
+		if(Ay < 0) {
+			while(Ay < 0) {
+				printf("\nDevice is flipped.\n");
+				Acc_y = read_raw_data(ACCEL_YOUT_H);
+				Ay = Acc_y/16384.0;
+				delay(1000);
+			}
+		}
+		
+		printf("\nDevice is fine.\n");
+		delay(1000);
+		
+	}
+	return 0;
+}
